0cb083002b93559fe320f55114c8e9000bfc3443
BackPicker <nico1a@naver.com>
2024-10-10 00:04:36 +0900

    review 쿼리 수정

--- review/src/main/java/com/example/review/repository/ReviewRepository.java
+++ review/src/main/java/com/example/review/repository/ReviewRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query("SELECT r FROM Review r WHERE r.product.id = :productId")
+    @Query("SELECT r FROM Review r JOIN FETCH r.user WHERE r.product.id = :productId ORDER BY r.id DESC")
     List<Review> findByProductId(@Param("productId") Long productId, Pageable pageable);
 
     @Query("SELECT new com.example.review.domain.dto.ReviewStatsDto(COUNT(r), AVG(r.score)) FROM Review r WHERE r.product.id = :productId")
