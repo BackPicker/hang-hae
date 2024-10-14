a4df397f63e4897224059388a23aa994a4be631f
BackPicker <nico1a@naver.com>
2024-10-14 11:19:33 +0900

    기본 기능 + 1초에 최대 500개의 요청 제한 완료

--- restock/src/main/java/com/example/restock/RestockApplication.java
+++ restock/src/main/java/com/example/restock/RestockApplication.java
@@ -5,7 +5,9 @@ import com.example.restock.repository.ProductUserNotificationRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class RestockApplication {
 
--- restock/src/main/java/com/example/restock/scheduler/Scheduler.java
+++ restock/src/main/java/com/example/restock/scheduler/Scheduler.java
@@ -5,6 +5,7 @@ import com.example.restock.repository.ProductRepository;
 import com.example.restock.service.ProductRestockNotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class Scheduler {
     private final ProductRestockNotificationService productRestockNotificationService;
     private final ProductRepository                 productRepository;
 
+    @Scheduled(cron = "*/1 * * * * *")
     public void updateQuantity() {
         List<Product> productList = productRepository.findAll();
         for (Product product : productList) {
